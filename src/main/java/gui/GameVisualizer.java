96720db6a82384dea5a3be91358d6b9a619b509e
Self-taugh <128119731+Self-taugh@users.noreply.github.com>
2025-03-25 22:57:03 +0500

    First Task
    Огромное мессиво в GameVisualisator разделено на 2 класса, каждый из который занимается своим делом. 1 класс решил не переименновывать для наглядности. Сложный код создания меню тоже упростил. Как исправить синусоидальное поведение червя пока не понял.

--- src/main/java/gui/GameVisualizer.java
+++ src/main/java/gui/GameVisualizer.java
@@ -32,9 +32,11 @@ public class GameVisualizer extends JPanel
     
     private static final double maxVelocity = 0.1; 
     private static final double maxAngularVelocity = 0.001; 
-    
+
+    private Visualizer visualizer;
     public GameVisualizer() 
     {
+        visualizer = new Visualizer();
         m_timer.schedule(new TimerTask()
         {
             @Override
@@ -162,31 +164,31 @@ public class GameVisualizer extends JPanel
     {
         return (int)(value + 0.5);
     }
-    
+
     @Override
     public void paint(Graphics g)
     {
         super.paint(g);
-        Graphics2D g2d = (Graphics2D)g; 
-        drawRobot(g2d, round(m_robotPositionX), round(m_robotPositionY), m_robotDirection);
-        drawTarget(g2d, m_targetPositionX, m_targetPositionY);
+        Graphics2D g2d = (Graphics2D)g;
+        double[] Cords = {m_robotPositionX, m_robotPositionY, m_robotDirection, m_targetPositionX, m_robotPositionY};
+        visualizer.paint(g2d, Cords);
     }
-    
+    /*
     private static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
     {
         g.fillOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
     }
-    
+
     private static void drawOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
     {
         g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
     }
-    
+
     private void drawRobot(Graphics2D g, int x, int y, double direction)
     {
-        int robotCenterX = round(m_robotPositionX); 
+        int robotCenterX = round(m_robotPositionX);
         int robotCenterY = round(m_robotPositionY);
-        AffineTransform t = AffineTransform.getRotateInstance(direction, robotCenterX, robotCenterY); 
+        AffineTransform t = AffineTransform.getRotateInstance(direction, robotCenterX, robotCenterY);
         g.setTransform(t);
         g.setColor(Color.MAGENTA);
         fillOval(g, robotCenterX, robotCenterY, 30, 10);
@@ -197,14 +199,14 @@ public class GameVisualizer extends JPanel
         g.setColor(Color.BLACK);
         drawOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
     }
-    
+
     private void drawTarget(Graphics2D g, int x, int y)
     {
-        AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0); 
+        AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0);
         g.setTransform(t);
         g.setColor(Color.GREEN);
         fillOval(g, x, y, 5, 5);
         g.setColor(Color.BLACK);
         drawOval(g, x, y, 5, 5);
-    }
+    }*/
 }
---
+++ src/main/java/gui/Visualizer.java
@@ -0,0 +1,61 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
+public class Visualizer extends JPanel {
+
+    // Cords[0] - m_robotPositionX    Cords[1] - m_robotPositionY
+    // Cords[2] - m_robotDirection   Cords[3] - m_targetPositionX   Cords[4] - m_targetPositionY
+    public void paint(Graphics g, double[] Cords)
+    {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D)g;
+        double[] Cord2 = {Cords[0], Cords[1], Cords[2]};
+        drawRobot(g2d,  Cord2);
+        drawTarget(g2d, (int)(Cords[3]), (int)(Cords[4]));
+    }
+
+    private static int round(double value)
+    {
+        return (int)(value + 0.5);
+    }
+
+    private static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
+    {
+        g.fillOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    }
+
+    private static void drawOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
+    {
+        g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    }
+
+    // Cords[0] - m_robotPositionX Cords[1] - m_robotPositionY Cords[2] - direction
+    private void drawRobot(Graphics2D g, double[] Cords)
+    {
+        int robotCenterX = round(Cords[0]);
+        int robotCenterY = round(Cords[1]);
+        AffineTransform t = AffineTransform.getRotateInstance(Cords[2], robotCenterX, robotCenterY);
+        g.setTransform(t);
+        g.setColor(Color.MAGENTA);
+        fillOval(g, robotCenterX, robotCenterY, 30, 10);
+        g.setColor(Color.BLACK);
+        drawOval(g, robotCenterX, robotCenterY, 30, 10);
+        g.setColor(Color.WHITE);
+        fillOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+        g.setColor(Color.BLACK);
+        drawOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+    }
+
+    private void drawTarget(Graphics2D g, int x, int y)
+    {
+        AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0);
+        g.setTransform(t);
+        g.setColor(Color.GREEN);
+        fillOval(g, x, y, 5, 5);
+        g.setColor(Color.BLACK);
+        drawOval(g, x, y, 5, 5);
+    }
+}
